fcdf8160086c10a5d6adf27437b4712d6fcf1bc1
lerodic <daniel@rodev.net>
2025-07-30 15:54:08 +0200

    feat(app): Implement interactive mode
    Add 'Mode' (abstract) and 'InteractiveMode' (concrete) classes; update inversify config

--- src/config/inversify/inversify.config.ts
+++ src/config/inversify/inversify.config.ts
@@ -2,6 +2,8 @@ import { Container } from "inversify";
 import TYPES from "./inversify.types";
 import Logger from "../../core/Logger";
 import RecordParser from "../../core/RecordParser";
+import Prompt from "../../core/Prompt";
+import InteractiveMode from "../../core/modes/InteractiveMode";
 
 function setupContainer(): Container {
   const container = new Container();
@@ -9,6 +11,10 @@ function setupContainer(): Container {
   container.bind<Logger>(TYPES.Logger).to(Logger);
 
   container.bind<RecordParser>(TYPES.RecordParser).to(RecordParser);
+
+  container.bind<Prompt>(TYPES.Prompt).to(Prompt);
+
+  container.bind<InteractiveMode>(TYPES.InteractiveMode).to(InteractiveMode);
 
   return container;
 }
--- src/config/inversify/inversify.types.ts
+++ src/config/inversify/inversify.types.ts
@@ -1,6 +1,8 @@
 const TYPES = {
   Logger: Symbol.for("Logger"),
   RecordParser: Symbol.for("RecordParser"),
+  Prompt: Symbol.for("Prompt"),
+  InteractiveMode: Symbol.for("InteractiveMode"),
 };
 
 export default TYPES;
