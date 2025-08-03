f54263d0deff0a9a4994abb50b53465383e15cd5
lerodic <daniel@rodev.net>
2025-08-03 20:47:52 +0200

    feat(app): Add schedule-related types to 'types.d.ts'

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import { DAYS_OF_WEEK } from "./config/constants";
 
 export interface ScriptExecutionOptions {
   script: string;
@@ -7,3 +8,38 @@ export interface ScriptExecutionOptions {
 }
 
 export type Program = Command;
+
+export type Action = "schedule" | "status" | "test" | "exit";
+
+export type ScheduleFrequency = "hourly" | "daily" | "weekly";
+
+export type Day = (typeof DAYS_OF_WEEK)[number];
+
+export interface BackupSchedule {
+  name: string;
+  frequency: ScheduleFrequency;
+  options: BackupScheduleOptions;
+}
+
+export interface BackupScheduleOptions {
+  time?: string;
+  day?: Day;
+  hours?: number;
+  outputPath?: string;
+}
+
+export type BackupScheduleOptionsDaily = Required<
+  Omit<BackupScheduleOptions, "hours">
+>;
+
+export type BackupScheduleOptionsWeekly = Required<BackupScheduleOptions>;
+
+export type BackupScheduleOptionsHourly = Pick<
+  Required<BackupScheduleOptions>,
+  "hours" | "outputPath"
+>;
+
+export type ScheduleOptions =
+  | BackupScheduleOptionsDaily
+  | BackupScheduleOptionsWeekly
+  | BackupScheduleOptionsHourly;
