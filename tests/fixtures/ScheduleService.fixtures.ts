f2db48ced0b07bebb4addfe2150a3d7b4dcc19b0
lerodic <daniel@rodev.net>
2025-08-07 17:00:14 +0200

    test(app): Update tests and fixtures for 'ScheduleService'

--- tests/fixtures/ScheduleService.fixtures.ts
+++ tests/fixtures/ScheduleService.fixtures.ts
@@ -60,3 +60,96 @@ export const removeNonExistingScheduleFixtures: RemoveScheduleFixtures[] = [
     expected: [],
   },
 ];
+
+export const doesScheduleExistTrueFixtures: {
+  name: string;
+  schedules: BackupSchedule[];
+}[] = [
+  {
+    name: "Test schedule",
+    schedules: [
+      {
+        name: "Test schedule",
+        frequency: "daily",
+        options: {
+          time: "02:00",
+          outputPath: "/Users/test/BearBackup",
+        },
+      },
+    ],
+  },
+  {
+    name: "Third schedule",
+    schedules: [
+      {
+        name: "First schedule",
+        frequency: "daily",
+        options: {
+          time: "02:00",
+          outputPath: "/Users/test/BearBackup",
+        },
+      },
+      {
+        name: "Second schedule",
+        frequency: "daily",
+        options: {
+          time: "03:00",
+          outputPath: "/Users/test/BearBackup",
+        },
+      },
+      {
+        name: "Third schedule",
+        frequency: "daily",
+        options: {
+          time: "04:00",
+          outputPath: "/Users/test/BearBackup",
+        },
+      },
+    ],
+  },
+];
+
+export const doesScheduleExistFalseFixtures: {
+  name: string;
+  schedules: BackupSchedule[];
+}[] = [
+  {
+    name: "Test_schedule",
+    schedules: [
+      {
+        name: "Test schedule",
+        frequency: "daily",
+        options: {
+          time: "02:00",
+          outputPath: "/Users/test/BearBackup",
+        },
+      },
+    ],
+  },
+  {
+    name: "Fourth schedule",
+    schedules: [
+      {
+        name: "First schedule",
+        frequency: "daily",
+        options: {
+          time: "02:00",
+        },
+      },
+      {
+        name: "Second schedule",
+        frequency: "daily",
+        options: {
+          time: "03:00",
+        },
+      },
+      {
+        name: "Third schedule",
+        frequency: "daily",
+        options: {
+          time: "04:00",
+        },
+      },
+    ],
+  },
+];
